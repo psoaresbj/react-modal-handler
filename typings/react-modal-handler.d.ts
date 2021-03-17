ed13fdc9efb6821658751cb40c0b438e70e7161e
Paulo Soares <paulo.soares@uphold.com>
2021-03-17 10:23:32 +0000

    Add types

---
+++ typings/react-modal-handler.d.ts
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+declare module 'react-modal-handler' {
+  function close(cb?: Function, options?: any);
+  function open(type: string, props?: any, options?: any);
+
+  export const modal = {
+    close,
+    open
+  };
+
+  type ModalManagerProps = {
+    modals: {[key: string]: any};
+    options?: any;
+  };
+
+  type ModalManagerState = any;
+
+  export class ModalManager extends React.Component<ModalManagerProps, ModalManagerState> {};
+}
