8e0f33d2d8715eea9adbc3c972c47b9d1d040e16
Paulo Soares <paulo.soares@uphold.com>
2021-03-17 11:19:10 +0000

    Add-type-declaration-to-files

--- typings/react-modal-handler.d.ts
+++ typings/react-modal-handler.d.ts
@@ -1,20 +1,18 @@
-declare module 'react-modal-handler' {
-  import * as React from 'react';
+import * as React from 'react';
 
-  function close(cb?: Function, options?: any);
-  function open(type: string, props?: any, options?: any);
+function close(cb?: Function, options?: any);
+function open(type: string, props?: any, options?: any);
 
-  export const modal = {
-    close,
-    open
-  };
+export const modal = {
+  close,
+  open
+};
 
-  type ModalManagerProps = {
-    modals: {[key: string]: any};
-    options?: any;
-  };
+type ModalManagerProps = {
+  modals: {[key: string]: any};
+  options?: any;
+};
 
-  type ModalManagerState = any;
+type ModalManagerState = any;
 
-  export class ModalManager extends React.Component<ModalManagerProps, ModalManagerState> {};
-}
+export class ModalManager extends React.Component<ModalManagerProps, ModalManagerState> {};
